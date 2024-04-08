a88255af92a1e805fbb66a56ebc43543864c1f0e
Oliver Schmidt <oliverlucaschmidt@gmail.com>
2024-04-08 18:15:03 +0200

    data ist da

---
+++ Aufgaben/Aufgabe_2/Code/data.hh
@@ -0,0 +1,17 @@
+#ifndef DATA_H // include guard
+#define DATA_H
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <stdexcept>
+#include <string>
+
+class get_Artikel {
+private:
+  void open_File();
+protected:
+public:
+};
+
+#endif
